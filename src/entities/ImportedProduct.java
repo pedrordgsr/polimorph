dd3fe3a4cd8c86c99e5201389b2ee114ad811c9f
Pedro Rodrigues <pedrorodriguesssrrr@gmail.com>
2024-06-10 13:16:40 -0300

    first

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class ImportedProduct extends Product {
+    protected Double customsFee;
+
+
+
+}
---
+++ src/entities/Product.java
@@ -0,0 +1,39 @@
+package entities;
+
+public class Product {
+    protected String name;
+    protected Double price;
+
+    public Product(){
+
+    }
+
+    public Product(String name, Double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String priceTag() {
+        final StringBuilder sb = new StringBuilder("Product{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", price=").append(price);
+        sb.append('}');
+        return sb.toString();
+    }
+}
