dcfddb83ac23a610cbcce1de3af5333513b175f3
Pedro Rodrigues <pedrorodriguesssrrr@gmail.com>
2024-06-10 13:36:58 -0300

    first

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,30 @@
 package application;
 
+import entities.Product;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of products: ");
+
+        int qnt = sc.nextInt();
+
+        for(int i = 1;i <= qnt;i++){
+            System.out.println("Enter product number " + 1 +" info");
+            System.out.println("Common or imported? (c/i)");
+            char type = sc.next().charAt(0);
+            if (type == 'c'){
+                System.out.println("Type the product name: ");
+                String name = sc.nextLine();
+                System.out.println("Type the product price: ");
+                Double price = sc.nextDouble();
+
+                Product prod = new Product(name,price);
+            }
+        }
+
 
     }
 }
--- src/entities/ImportedProduct.java
+++ src/entities/ImportedProduct.java
@@ -3,6 +3,30 @@ package entities;
 public class ImportedProduct extends Product {
     protected Double customsFee;
 
+    public ImportedProduct() {
 
+    }
 
+    public ImportedProduct(String name, Double price, Double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public Double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    @Override
+    public String priceTag() {
+        final StringBuilder sb = new StringBuilder("Product{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", price=").append(price);
+        sb.append('}');
+        sb.append("Customs fee: " + getCustomsFee());
+        return sb.toString();
+    }
 }
